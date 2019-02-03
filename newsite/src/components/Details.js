2aa4badfedff94ec6fa756eca047046bab8eb61d
Jenny Bonsak <jenny.bonsak@gmail.com>
2019-02-03 21:37:17 +0100

    Add loading symbol

--- newsite/src/components/Details.js
+++ newsite/src/components/Details.js
@@ -1,11 +1,17 @@
 import React from "react";
+import loading from '../../assets/icons/loading.svg';
+
 
 const Details = () => {
     
     return(
     <main>
       <div>
-          ASTEROID DETAILS
+          <h2>Asteroid Details</h2>
+          <span className="loading-wrapper">
+            <img className="loading__icon" src={ loading } />
+            <p>Loading...</p>
+          </span>
       </div>
     </main>
     );
